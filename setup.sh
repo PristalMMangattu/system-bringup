45af91901e60409624bb32520dabb71e8a214c4b
Pristal M Mangattu <pristalmangattu@gmail.com>
2023-09-03 13:52:46 +0530

    Included some python data science packages installations.

---
+++ setup.sh
@@ -0,0 +1,16 @@
+
+# Data processing python libraries
+pip install numpy
+pip install pandas
+pip install statsmodels
+pip install scipy
+pip install scikit-learn
+pip install keras
+pip install tensorflow
+pip install torch
+
+# plotting libraries
+pip install matplotlib
+pip install seaborn
+pip install plotly
+
